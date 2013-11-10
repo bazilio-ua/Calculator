a2b206a275c18eb26decbfc5cae8a5a1c707cd95
Basil Nikityuk <basil.nikityuk@gmail.com>
2013-11-10 06:48:34 +0200

    Initial Commit

---
+++ Calculator/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  Calculator
+//
+//  Created by Basil Nikityuk on 11/10/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
